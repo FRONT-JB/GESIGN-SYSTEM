03a0e6fb631a805471af36166850b592903e5f65
Genie <jeonbg@kakao.com>
2022-05-13 23:11:17 +0900

    Setup : Eslint, Prettier Configs

---
+++ .eslintrc.js
@@ -0,0 +1,87 @@
+const path = require('path');
+
+module.exports = {
+  extends: ['airbnb', 'prettier'],
+  plugins: ['prettier', 'react-hooks'],
+  rules: {
+    'prettier/prettier': 'error',
+    'import/no-extraneous-dependencies': [
+      'error',
+      {
+        devDependencies: [
+          '**/*.stories.tsx',
+          '**/webpack.*.js',
+          '**/script/*.js',
+          '**/.storybook/*.@(js|ts)?(x)',
+        ],
+      },
+    ],
+    'react-hooks/rules-of-hooks': 'error',
+    'react-hooks/exhaustive-deps': 'warn',
+    'react/react-in-jsx-scope': 'off',
+    'react/jsx-props-no-spreading': 'off',
+    'react/jsx-filename-extension': ['error', { extensions: ['.tsx', '.jsx'] }],
+    'react/function-component-definition': 'off',
+    'import/extensions': [
+      'error',
+      'ignorePackages',
+      {
+        js: 'never',
+        mjs: 'never',
+        jsx: 'never',
+        ts: 'never',
+        tsx: 'never',
+      },
+    ],
+    'jsx-a11y/label-has-associated-control': [
+      'error',
+      { controlComponents: ['input', 'select'] },
+    ],
+  },
+  overrides: [
+    {
+      files: ['**/*.ts?(x)'],
+      parser: '@typescript-eslint/parser',
+      extends: [
+        'plugin:@typescript-eslint/recommended',
+        'plugin:@typescript-eslint/recommended-requiring-type-checking',
+      ],
+      rules: {
+        'react/prop-types': 'off',
+        'react/require-default-props': 'off',
+        '@typescript-eslint/explicit-module-boundary-types': 'off',
+        'no-use-before-define': 'off',
+        '@typescript-eslint/no-use-before-define': [
+          'error',
+          { variables: false },
+        ],
+        'no-useless-constructor': 'off',
+        '@typescript-eslint/no-useless-constructor': 'error',
+        '@typescript-eslint/no-floating-promises': 'off',
+      },
+      parserOptions: {
+        project: ['./tsconfig.json', './packages/**/tsconfig.json'],
+      },
+    },
+    {
+      files: ['packages/ui/**/*.ts?(x)', 'packages/ui/**/*.js?(x)'],
+      settings: {
+        'import/resolver': {
+          typescript: {
+            project: path.resolve(`${__dirname}/packages/ui/tsconfig.json`),
+          },
+        },
+      },
+    },
+    {
+      files: ['apps/nextjs/**/*.ts?(x)', 'apps/nextjs/**/*.js?(x)'],
+      settings: {
+        'import/resolver': {
+          typescript: {
+            project: path.resolve(`${__dirname}/apps/nextjs/tsconfig.json`),
+          },
+        },
+      },
+    },
+  ],
+};
